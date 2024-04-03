65352456182346ae9b88c1eba228c16a517dcd79
user <user@DESKTOP-27BS83H>
2024-04-03 11:44:50 +0900

    weak5

---
+++ javaPrograming/src/weak5/weak5.java
@@ -0,0 +1,17 @@
+package weak5;
+
+public class weak5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 3;
+			int b = 5;
+			
+			if(a > b) {
+				System.out.println("참입니다.");
+			}else {
+				System.out.println("휴강입니다.");
+			}
+	}
+
+}
